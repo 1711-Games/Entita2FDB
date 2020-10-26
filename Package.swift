62c24cf39db55330f6c433f8f42b773c1e683c55
Kirill Titov <work@kirilltitov.com>
2020-10-26 15:04:57 +0300

    Entita2 dependency private >> public

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         .library(name: "Entita2FDB", targets: ["Entita2FDB"]),
     ],
     dependencies: [
-        .package(url: "git@github.com:1711-Games/Entita2.git", .branch("master")),
+        .package(url: "https://github.com/1711-Games/Entita2.git", .upToNextMinor(from: "0.1.0")),
         .package(url: "https://github.com/kirilltitov/FDBSwift.git", .upToNextMajor(from: "4.0.0")),
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.19.0"),
     ],
