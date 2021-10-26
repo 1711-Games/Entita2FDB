910001f189362eb88561f247f9038d7546457074
Kirill Titov <work@kirilltitov.com>
2021-10-26 12:54:07 +0300

    Bump platform version to v12

--- Package.swift
+++ Package.swift
@@ -4,14 +4,14 @@ import PackageDescription
 
 let package = Package(
     name: "Entita2FDB",
-    platforms: [.macOS(.v10_15)],
+    platforms: [.macOS(.v12)],
     products: [
         .library(name: "Entita2FDB", targets: ["Entita2FDB"]),
     ],
     dependencies: [
         .package(url: "https://github.com/1711-Games/Entita2.git", from: "1.0.0"),
-        .package(url: "https://github.com/kirilltitov/FDBSwift.git", .upToNextMajor(from: "5.0.0-RC-2")),
-        .package(url: "https://github.com/1711-Games/LGN-Log.git", .upToNextMinor(from: "0.2.0")),
+        .package(url: "https://github.com/kirilltitov/FDBSwift.git", .upToNextMajor(from: "5.0.0-RC-3")),
+        .package(url: "https://github.com/1711-Games/LGN-Log.git", .upToNextMinor(from: "0.4.0")),
     ],
     targets: [
         .target(
